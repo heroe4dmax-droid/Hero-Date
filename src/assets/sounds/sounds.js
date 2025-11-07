edf2a83f2cd44081a0c5ce042356e76eb6a34daf
heroe4dmax-droid <heroe4dmax@gmail.com>
2025-11-07 17:46:38 -0500

    Add files via upload

---
+++ src/assets/sounds/sounds.js
@@ -0,0 +1,10 @@
+// src/config/sounds.js
+
+export const Sounds = {
+  NOTIFICATION: require('../assets/sounds/notification.mp3'),
+  MESSAGE: require('../assets/sounds/message.mp3'),
+  LIKE: require('../assets/sounds/like.mp3'),
+  MATCH: require('../assets/sounds/match.mp3'),
+  BUTTON_CLICK: require('../assets/sounds/click.mp3'),
+  BACKGROUND_MUSIC: require('../assets/sounds/background.mp3'),
+};
