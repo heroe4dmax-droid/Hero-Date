e0e7c56b3717e427e5cda3609af83483853c7cb0
heroe4dmax-droid <heroe4dmax@gmail.com>
2025-11-07 17:41:47 -0500

    Add files via upload

---
+++ src/assets/images/images.js
@@ -0,0 +1,23 @@
+// src/config/images.js
+
+export const Images = {
+  LOGO: require('../assets/images/logo.png'),
+  DEFAULT_AVATAR: require('../assets/images/default_avatar.png'),
+  PLACEHOLDER: require('../assets/images/placeholder.png'),
+  BACKGROUND: require('../assets/images/background.jpg'),
+  EMPTY_STATE: require('../assets/images/empty_state.png'),
+
+  // Example for multiple avatars
+  AVATARS: {
+    AVATAR_1: require('../assets/images/avatars/avatar1.png'),
+    AVATAR_2: require('../assets/images/avatars/avatar2.png'),
+    AVATAR_3: require('../assets/images/avatars/avatar3.png'),
+  },
+
+  // Social logos
+  SOCIAL: {
+    FACEBOOK: require('../assets/images/social/facebook.png'),
+    GOOGLE: require('../assets/images/social/google.png'),
+    TWITTER: require('../assets/images/social/twitter.png'),
+  },
+};
